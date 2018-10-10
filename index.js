d89d22b9216924571e53f43303001c467b2abf75
EVERTZ_MICROSYS\bwang <bwang@evertz.com>
2018-10-10 13:02:24 -0400

    fixed serving static files

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').Server(app);
 
 const PORT = 8080;
 
+app.use(express.static(__dirname + '/public/'));
+
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/src/html/index.html');
+	res.sendFile(__dirname + '/public/html/index.html');
 });
 
 http.listen(PORT, () => {
